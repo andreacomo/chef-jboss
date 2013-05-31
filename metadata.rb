281b1b876669d881162531d95eea53db99d2ed6d
cxz <alexandre.maia@gmail.com>
2013-05-30 23:00:16 -0400

    version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache v2.0"
 description      "Installs/Configures jboss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.13"
+version          "0.1.0"
 name	         "jboss"
 
 %w{ java logrotate sudo }.each do |cb|
@@ -12,6 +12,6 @@ end
 
 recipe "jboss", "installs jboss from the jboss community site"
 
-%w{ debian ubuntu centos redhat fedora }.each do |os|
+%w{ ubuntu centos }.each do |os|
   supports os
 end
